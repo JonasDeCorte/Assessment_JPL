49715a41b1209a37368e4ba0df879b6dbd1c9977
Jonas De Corte <Jonas.DeCorte@inetum-realdolmen.world>
2023-04-30 14:27:34 +0200

    display all stats when not null / 0

--- Frontend/clubbrugge-erp/src/components/match.details.tsx
+++ Frontend/clubbrugge-erp/src/components/match.details.tsx
@@ -1,47 +1,54 @@
+// MatchDetails.tsx
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { MATCH_PLAYER_STATS_QUERY } from '../matchPlayerStatsQuery.tsx';
-import './match.details.css';
-// MatchDetails.tsx
+import styles from './match.details.css';
+
 const MatchDetails: React.FC = () => {
     const { matchId } = useParams<{ matchId: string }>();
     const { loading, error, data } = useQuery(MATCH_PLAYER_STATS_QUERY, {
       variables: { matchId },
     });
   
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
+    if (loading) return <p className={styles.loading}>Loading...</p>;
+    if (error) return <p className={styles.error}>Error :(</p>;
   
-    return (
-        <div className="match-details">
-          {data.matchPlayerStats.map((player: any) => (
-            <div key={player.id} className="player-stats">
-              <h3>{player.playerName} ({player.teamName})</h3>
-              <p>Minutes: {player.playerMatchMinutes}</p>
-              <p>Non-penalty xG per shot: {player.playerMatchNpXgPerShot}</p>
-              <p>Non-penalty xG: {player.playerMatchNpXg}</p>
-              <p>Non-penalty shots: {player.playerMatchNpShots}</p>
-              <p>Goals: {player.playerMatchGoals}</p>
-              <p>Non-penalty goals: {player.playerMatchNpGoals}</p>
-              <p>xA: {player.playerMatchXa}</p>
-              <p>Key passes: {player.playerMatchKeyPasses}</p>
-              <p>Opportunity-creating key passes: {player.playerMatchOpKeyPasses}</p>
-              <p>Assists: {player.playerMatchAssists}</p>
-              <p>Through balls: {player.playerMatchThroughBalls}</p>
-              <p>Passes into box: {player.playerMatchPassesIntoBox}</p>
-              <p>Opportunity-creating passes into box: {player.playerMatchOpPassesIntoBox}</p>
-              <p>Touches inside box: {player.playerMatchTouchesInsideBox}</p>
-              <p>Tackles: {player.playerMatchTackles}</p>
-              <p>Interceptions: {player.playerMatchInterceptions}</p>
-              <p>Possession: {player.playerMatchPossession}</p>
-              <p>Dribbled past: {player.playerMatchDribbledPast}</p>
-              <p>Dribbles faced: {player.playerMatchDribblesFaced}</p>
-            </div>
-          ))}
-        </div>
-      );
-    };
+    const copiedData = [...data.matchPlayerStats];
+    const sortedStats = copiedData.sort((a, b) => a.teamId - b.teamId);
+    const homeTeam = sortedStats.filter((stat) => stat.teamId === sortedStats[0].teamId);
+    const awayTeam = sortedStats.filter((stat) => stat.teamId !== sortedStats[0].teamId);
+  
+    const renderPlayerStats = (playerStats) =>
+    playerStats.map((stat) => (
+      <div key={stat.id} className="player-stats">
+        <h3>{stat.playerName}</h3>
+        {Object.entries(stat).map(([key, value]) => {
+            if (key !== '__typename' && value !== null && value !== 0) {
+                return (
+              <div key={key} className="stats">
+                {key}: {value}
+              </div>
+            );
+          }
+          return null;
+        })}
+      </div>
+    ));
+
+  return (
+    <div className="match-details">
+      <div className="homeTeam">
+        <h2>Home Team</h2>
+        {renderPlayerStats(homeTeam)}
+      </div>
+      <div className="awayTeam">
+        <h2>Away Team</h2>
+        {renderPlayerStats(awayTeam)}
+      </div>
+    </div>
+  );
+};
+  
   
   export default MatchDetails;
-  
